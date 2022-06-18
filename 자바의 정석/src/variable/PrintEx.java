2d9b2b7f06f8cacaeb33cb9fe6dd6ffc167064a2
codekim97 <kimtaeu4616@gmail.com>
2022-06-18 20:11:08 +0900

    2.1 변수의 타입 실습해보기

---
+++ 자바의 정석/src/variable/PrintEx.java
@@ -0,0 +1,25 @@
+package variable;
+
+import java.util.Scanner;
+
+public class PrintEx {
+    public static void main(String[] args) {
+        String url = "www.naver.com";
+
+        int a = 10;
+        float f1 = .10f;
+        float f2 = 1e1f;
+        float f3 = 3.14e3f;
+        double d = 1.234556789;
+
+        //줄 간격 맞추기
+        System.out.printf("a = [%05d]\n", a);
+        System.out.printf("f1 = [%10f]\n", f1);
+
+        //화면에서 입력 받기
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("input = " + input);
+
+    }
+}
---
+++ 자바의 정석/src/variable/Variable.java
@@ -0,0 +1,31 @@
+package variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        /**
+         * 변수의 선언
+         */
+        Boolean trueOrFalse = true;
+        char a = 'A';
+        int b = 30;
+        long c = 30L; //long 형에는 L을 붙여줘야 함
+        float d = 30F; // float 형에는 F를 붙여줘야함
+        double f = 30;
+
+        System.out.println("trueOrFalse = " + trueOrFalse);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("f = " + f);
+
+
+        /**
+         * 상수
+         */
+        final int SUM = 100;
+        final int COUNT = 5;
+        int average = SUM / COUNT;
+
+    }
+}
